8a38ff82253bc1de514c4e2e02c8d503b17f1c5e
Hilal Habeeb <118432608+hilalhabeeb@users.noreply.github.com>
2023-11-09 13:41:32 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
+
+
+
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
@@ -80,3 +83,17 @@ class TurfListing(models.Model):
 
     def __str__(self):
         return self.turf_name
+    
+
+class Booking(models.Model):
+    user = models.ForeignKey(Usertable, on_delete=models.CASCADE, related_name='bookings')
+    turf_listing = models.ForeignKey(TurfListing, on_delete=models.CASCADE)
+    turf_provider = models.ForeignKey(TurfProvider, on_delete=models.CASCADE)
+    booking_date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    total_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Booking for {self.turf_listing.turf_name} by {self.user.email}"
